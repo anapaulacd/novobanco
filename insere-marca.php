9087b122c7d3bdad6075eef373f447ba43af4f21
anapaulacd <127848114+anapaulacd@users.noreply.github.com>
2024-08-07 00:12:37 -0300

    Update insere-marca.php  ,,,,,,

--- insere-marca.php
+++ insere-marca.php
@@ -1,12 +1,13 @@
 <?php
+*/ inclui o arquivo `conexao.php`, que provavelmente contém a lógica de conexão com o banco de dados.
 include('controller/conexao.php');
-
+*/ metodo post
 $descricao = $_POST['descricao'];
 
 echo "<h3>Descrição: $descricao</h3>";
 
 $cad_marca= "INSERT INTO marca(Descricao) VALUES ('$descricao')";
-
+*/ mysqli_query() em PHP executa uma consulta SQL em um banco de dados MySQL. Faz parte da extensão MySQLi
 if(mysqli_query($mysqli, $cad_marca)){
     echo "<h1>marca cadastrada com sucesso!</h1><br>";
     
